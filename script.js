5d5c7d9c0c224716b6091a586c76680fc89ed746
ronojoyd <ron2020d@gmail.com>
2024-07-07 15:01:59 -0400

    Added time functionality using JS

---
+++ script.js
@@ -0,0 +1,8 @@
+// Update time in menu bar
+function updateTime() {
+    const now = new Date();
+    document.getElementById('current-time').textContent = now.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+}
+setInterval(updateTime, 1000);
+updateTime();
+
